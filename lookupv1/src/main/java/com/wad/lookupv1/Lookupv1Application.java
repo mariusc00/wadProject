7f3a11142c5618e71905f2a0cd1c85409e014aab
Marius Croitoru <93078160+mariusc00@users.noreply.github.com>
2022-05-02 20:57:28 +0300

    Add files via upload

---
+++ lookupv1/src/main/java/com/wad/lookupv1/Lookupv1Application.java
@@ -0,0 +1,13 @@
+package com.wad.lookupv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lookupv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lookupv1Application.class, args);
+    }
+
+}
---
+++ lookupv1/src/test/java/com/wad/lookupv1/Lookupv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wad.lookupv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lookupv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
