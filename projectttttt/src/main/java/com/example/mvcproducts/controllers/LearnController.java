09f2fb300131c1477036fc216e8f9f0f9df52489
Marius Croitoru <93078160+mariusc00@users.noreply.github.com>
2022-05-17 15:40:39 +0300

    update again

--- projectttttt/src/main/java/com/example/mvcproducts/controllers/LearnController.java
+++ projectttttt/src/main/java/com/example/mvcproducts/controllers/LearnController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
-public class HomeController {
+@RequestMapping("/learn")
+public class LearnController {
 
-  @GetMapping
-  public String home(){
-    return "home";
-  }
+    @GetMapping
+    public String learn(){
+        return "learn";
+    }
 }
--- projectttttt/src/main/java/com/example/mvcproducts/controllers/ShopController.java
+++ projectttttt/src/main/java/com/example/mvcproducts/controllers/ShopController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
-public class HomeController {
+@RequestMapping("/shop")
+public class ShopController {
 
-  @GetMapping
-  public String home(){
-    return "home";
-  }
+    @GetMapping
+    public String shop(){
+        return "shop";
+    }
 }
